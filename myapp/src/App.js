c47bf9dd2a4f21eef2926c3abdfa2b044b6c558f
Vimlesh kumari <vimlesh3012@gmail.com>
2024-09-26 23:14:15 +0530

    items css updated

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,13 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './components/counter';
 import LandingPage from './components/landingPage';
+import FoodProvider from './context/FoodProvider';
+import { ChakraProvider } from '@chakra-ui/react'
+import {
+  ChakraBaseProvider,
+  extendBaseTheme,
+  theme as chakraTheme,
+} from '@chakra-ui/react'
+
+const { Button } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+})
+
+
+
 
 function App() {
   return (
+    <ChakraBaseProvider theme={theme}>
+
+    <FoodProvider>
+
     <div className="App">
         <LandingPage/>
         {/* <Counter/> */}
     </div>
+    </FoodProvider>
+    </ChakraBaseProvider>
+
+
   );
 }
 
