5024b1cbb7c6464fff8fb2fc805aafdff57a50d9
Vimlesh kumari <vimlesh3012@gmail.com>
2024-09-26 23:14:31 +0530

    card added

--- myapp/src/pages/cart.js
+++ myapp/src/pages/cart.js
@@ -1,4 +1,27 @@
-export default function Cart(params) {
-    return <div>Cart</div>
-    
-}
+// src/components/Cart.js
+import React, { useContext } from 'react';
+import FoodContext from '../context/FoodContext';
+
+export default function Cart(params){
+  const { cart, removeFromCart } = useContext(FoodContext);
+
+
+  return (
+    <div>
+      <h2>Your Cart</h2>
+      {cart.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        cart.map((item) => (
+          <div key={item.id} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px 0' }}>
+            <h3>{item.name}</h3>
+            <p>Price: ${item.cost }</p>
+            <button onClick={() => removeFromCart(item.id)}>Remove from Cart</button>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+;
--- myapp/src/pages/header.js
+++ myapp/src/pages/header.js
@@ -1,14 +1,30 @@
 // import cart from "../../public/images/cart.jpg"
+import { useContext } from "react";
 import cart1 from "../Assets/cart1.png"
+import FoodContext from "../context/FoodContext";
+import Cart from "./cart";
+// import {useNavigate} from 'react-dom'
+
 export default function Header(params) {
+    const { cart } = useContext(FoodContext);
+    // const navigate = useNavigate();
+
+    const callCart = () => {
+        console.log("cart callled");
+        // navigate('/cart')
+        // return <Cart/>
+    }
+
     let count = 0
     return (
         <>
-            <div className={"Header-main"}>
+            {<div className={"Header-main"}>
                 <div>Home Food</div>
-                <div >
-                    <a onClick={() => { }} href="#" className="cart-main" style={{ textDecoration: "none" }}><img src={cart1} className="cart-image" /><span>Cart {count}</span></a></div>
-            </div>
+                <div onClick={() => { callCart() }} className="cart-main">
+                    <img src={cart1} className="cart-image" width={{ sm:10, md: 40 }}
+                    /><span >Cart {cart.length}</span></div>
+            </div>}
+
 
         </>
     )
