68c1938d659974a704ce5e2183ff213adc82501c
Vimlesh kumari <vimlesh3012@gmail.com>
2024-09-27 20:51:37 +0530

    modal added

--- myapp/src/pages/cart.js
+++ myapp/src/pages/cart.js
@@ -1,14 +1,21 @@
 // src/components/Cart.js
 import React, { useContext, useEffect } from 'react';
 import FoodContext from '../context/FoodContext';
-import { Box, Flex, Image, Text, Button, Center } from '@chakra-ui/react';
+import { Box, Flex, Image, Text, Button, Center, useDisclosure } from '@chakra-ui/react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react'
 
 
 export default function Cart(params) {
     const { cart = [], removeFromCart } = useContext(FoodContext);
     let sum = 0;
-    console.log("cart", cart);
-
     const total = () => {
         cart?.forEach(item => {
             sum = sum + item.cost
@@ -16,37 +23,63 @@ export default function Cart(params) {
         return sum
 
     }
+    return function BasicUsage() {
+        const { isOpen, onOpen, onClose } = useDisclosure()
+        return (
+            <>
+                {isOpen?
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Modal Title</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            {/* <Lorem count={2} /> */}
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                Close
+                            </Button>
+                            <Button variant='ghost'>Secondary Action</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>:"no item"}
+            </>
+        )
+    }
+    // return (
+    //     <>
 
 
 
-    return (
-        <>
-            <h2 >Your Cart</h2>
-            <Box display={"flex"} justifyContent={"center"}>
+    //         {
+    //         <h2 >Your Cart</h2>
+    //         <Box display={"flex"} justifyContent={"center"}>
 
-                {cart.length === 0 ? (
-                    <p>Your cart is empty.</p>
-                ) : <Box>
-                    {cart.map((item) => (
-                        <Box key={item.id} style={{ border: '1px solid #ccc', padding: '10px' }} width={400} m={8}>
-                            <h3>{item.name}</h3>
-                            <p>Price: ${item.cost}</p>
-                            <button onClick={() => removeFromCart(item.id)}>Remove from Cart</button>
-                        </Box>
-                    ))
-                    }
-                    <Box>
-                        <Box >
-                            <h3>Total</h3>
-                            <p>Total: {total()}</p>
-                        </Box>
+    //             {cart.length === 0 ? (
+    //                 <p>Your cart is empty.</p>
+    //             ) : <Box>
+    //                 {cart.map((item) => (
+    //                     <Box key={item.id} style={{ border: '1px solid #ccc', padding: '10px' }} width={400} m={8}>
+    //                         <h3>{item.name}</h3>
+    //                         <p>Price: ${item.cost}</p>
+    //                         <button onClick={() => removeFromCart(item.id)}>Remove from Cart</button>
+    //                     </Box>
+    //                 ))
+    //                 }
+    //                 <Box>
+    //                     <Box >
+    //                         <h3>Total</h3>
+    //                         <p>Total: {total()}</p>
+    //                     </Box>
 
-                    </Box>
-                </Box>
-                }
-            </Box>
-        </>
-    );
+    //                 </Box>
+    //             </Box>
+    //             }
+    //         </Box>}
+    //     </>
+    // );
 };
 
 ;
