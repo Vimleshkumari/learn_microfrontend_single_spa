9e32c68e713b7f449f7f0e46116740cddaadf3ef
Vimlesh kumari <vimlesh3012@gmail.com>
2024-09-30 23:09:22 +0530

    item updated

--- myapp/src/pages/addItems.js
+++ myapp/src/pages/addItems.js
@@ -1,38 +1,23 @@
 
 // import foodItems from "../utils/items.json"
 import FoodContext from '../context/FoodContext';
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Box, Flex, Image, Text, Button } from '@chakra-ui/react';
+import { FaStar } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export default function AddMenu(params) {
-    const { foodItems, removeFromCart, addToCart ,isCartClicked} = useContext(FoodContext);
 
-    // console.log("json", foodItems);
+export default function AddMenu({ totalStars = 5 }) {
+    const { foodItems, removeFromCart, addToCart, isCartClicked } = useContext(FoodContext);
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+    const notify = () => toast("Item added!", {
+        autoClose: false, // Don't auto-close
+        closeOnClick: true, // Enable click to close
+        closeButton: true // Show close button (the cross icon)
+    });
 
-    let items = [{
-        id: 1,
-        name: "Pizza",
-        cost: 100,
-        isGood: true
-    },
-    {
-        id: 2,
-        name: "Sandwich",
-        cost: 60,
-        isGood: true
-    },
-    {
-        id: 3,
-        name: "Burger",
-        cost: 40,
-        isGood: true
-    },
-    {
-        id: 4,
-        name: "Pulao",
-        cost: 80,
-        isGood: true
-    }]
 
     const importAllImages = (context) => {
         let images = {};
@@ -47,20 +32,54 @@ export default function AddMenu(params) {
 
 
     return (
+
         <>
             <Box className="main-menu-header">
                 <Box >Heyy your cart is hungary...</Box>
 
                 {foodItems?.map((item, ind) => {
-                    return <Box className="menu-card">
-                        <Box className="menu-items" key={ind}>
-                            <Box width={{sm:"100%",md: 80 }}
-                            ><Image src={images[item.url]} width={{sm:"100%",md: 80 }}/></Box>
-                            <Box style={{ display: "flex", justifyContent: "center" }}>
+                    return <Box className="menu-card" key={ind}>
+                        <Box className="menu-items" display={"flex"} flexDirection={{ sm: "column", md: "row" }}>
+                            <Box width={{ sm: "100%", md: 80 }}>
+                                <Image src={images[item.url]} width={{ sm: "100%", md: 80 }} />
+                            </Box>
+                            <Box style={{ display: "flex", justifyContent: "center" }} alignItems={"center"}>
+                                {[...Array(totalStars)].map((_, index) => {
+                                    const ratingValue = ind + 1;
+                                    return <FaStar
+                                        key={index}
+                                        size={6}
+                                        color={ratingValue <= (rating) ? "#ffc107" : "#e4e5e9"}
+                                        onClick={() => setRating(ratingValue)}
+                                        onMouseEnter={() => setHover(ratingValue)}
+                                        onMouseLeave={() => setHover(0)}
+                                    />
+                                })}
+
                                 <Box style={{ width: "150px" }}>{item.name}</Box>
-                                <Box style={{ width: "50px" }} >{item.cost}  </Box>
-                                <Box style={{ border: "2px solid  green", padding: "1px 8px", marginLeft: "5px" }} onClick={() => addToCart(item)}>+</Box>
-                                <Box style={{ border: "2px solid red", padding: "1px 8px", marginLeft: "5px" }} onClick={() => { removeFromCart(item.id) }}>-</Box>
+                                <Box style={{ width: "50px" }} ><span>&#8377;</span>{`${item.cost}`}  </Box>
+                                <Box style={{ border: "2px solid  green", borderRadius: "30%", padding: "1px 8px", marginLeft: "5px" }} onClick={() => {
+                                    notify();
+                                    addToCart(item)
+                                }}>+</Box>
+                                {/* <button onClick={notify}>Notify!</button> */}
+                                <ToastContainer
+                                    success
+                                    position="top-right"
+                                    autoClose={5000}
+                                    hideProgressBar={false}
+                                    newestOnTop={false}
+                                    closeOnClick={false}
+                                    rtl={false}
+                                    pauseOnFocusLoss
+                                    draggable
+                                    pauseOnHover
+                                    theme="colored"
+                                    transition="Bounce"
+                                />
+                                {/* Same as */}
+                                <ToastContainer />
+                                <Box style={{ border: "2px solid red", borderRadius: "30%", padding: "1px 8px", marginLeft: "5px" }} onClick={() => { removeFromCart(item.id) }}>-</Box>
                             </Box>
                         </Box>
                     </Box>
