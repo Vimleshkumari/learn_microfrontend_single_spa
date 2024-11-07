dec480bb4986b1037053b7a8b1d391cc50555201
Vimlesh kumari <vimlesh3012@gmail.com>
2024-11-07 12:46:59 +0530

    toaster added

--- myapp/src/context/FoodProvider.js
+++ myapp/src/context/FoodProvider.js
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 import FoodContext from './FoodContext';
 import foodItemsList from "../utils/items.json";
 import { useToast } from "@chakra-ui/react";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const FoodProvider = ({ children }) => {
     // State for managing food items and cart
     const [foodItems, setFoodItems] = useState(foodItemsList);
@@ -11,33 +12,36 @@ const FoodProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
     const [isCartClicked, setCartClicked] = useState([]);
 
-    
+
     const toast = useToast();
 
 
-      const cartClicked =()=>{
+    const cartClicked = () => {
         setCartClicked(!isCartClicked)
-      }
+    }
+
+    const notify = () => toast("Wow so easy!");
 
     // Function to add an item to the cart
     const addToCart = (item) => {
+        // notify();
         // console.log("item---", item);
         setCart((prevCart) => [...prevCart, item]);
-        toast({
-            position: "top-right",
-            title: "Item added.",
-            description: "We've created your account for you.",
-            status: "success",
-            // duration: 9000,
-            isClosable: false,
-            containerStyle: {
-                width: "200px", // Custom width
-                height: "50px" ,// Custom height
-                // backgroundColor:"green",
-                color:"green"
+        // toast({
+        //     position: "top-right",
+        //     title: "Item added.",
+        //     description: "We've created your account for you.",
+        //     status: "success",
+        //     // duration: 9000,
+        //     isClosable: false,
+        //     containerStyle: {
+        //         width: "200px", // Custom width
+        //         height: "50px" ,// Custom height
+        //         // backgroundColor:"green",
+        //         color:"green"
 
-            },
-        })
+        //     },
+        // })
     };
 
     // Function to remove an item from the cart
